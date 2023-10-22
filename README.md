f30ed9804db9f25a1f1d56063aa612f6f80718a1
piotr <piotrkalotaspam@gmail.com>
2023-10-22 16:06:34 +0200

    Add guest session usage

--- README.md
+++ README.md
@@ -6,8 +6,14 @@ To install required packages run:
 pip install -r requirements.txt
 ```
 
-To run the application:
+To run the application as using a guest session run the following command:
 
 ```bash
-python src/main.py [twitter_email] [twitter_username] [twitter_password]
+python src/main.py guest
+```
+
+Or to run the application using a twitter account run the following command:
+
+```bash
+python src/main.py account [twitter_email] [twitter_username] [twitter_password]
 ```
--- src/main.py
+++ src/main.py
@@ -4,11 +4,20 @@ from twitter.scraper import Scraper
 from tweetExtractor import getPoliticiansTweets
 from userExtractor import updateAndGetPoliticians
 
-if len(sys.argv) < 4:
-    print("Incorrect invocation. Usage: python main.py [twitter_email] [twitter_username] [twitter_password]")
-    exit(0)
+scraper = None
 
-scraper = Scraper(session=init_session(), save=False)
+if len(sys.argv) < 2:
+    print("Please provide a command scraper type - `python main.py guest` or `python main.py account`")
+    exit()
+
+if sys.argv[1] == "guest":
+    scraper = Scraper(session=init_session(), save=False)
+else:
+    if len(sys.argv) < 5:
+        print("Incorrect invocation. Usage: python main.py account [twitter_email] [twitter_username] [twitter_password]")
+        exit()
+    scraper = Scraper(sys.argv[2], sys.argv[3], sys.argv[4], save=False)
+
 politicians = updateAndGetPoliticians(scraper)
 succeded = getPoliticiansTweets(scraper, politicians)
 infoMessage = "Scraping finished successfully" if succeded else "Failed to scrape all politicians due to the rate limit being reached."
