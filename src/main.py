8d5330d3c8af30266c83c665285112b3ccf15e02
piotr <piotrkalotaspam@gmail.com>
2023-10-22 12:25:43 +0200

    Use guest session instead of own account

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+from twitter.util import init_session
 from twitter.scraper import Scraper
 from tweetExtractor import getPoliticiansTweets
 from userExtractor import updateAndGetPoliticians
@@ -7,13 +8,10 @@ if len(sys.argv) < 4:
     print("Incorrect invocation. Usage: python main.py [twitter_email] [twitter_username] [twitter_password]")
     exit(0)
     
-TWITTER_EMAIL = sys.argv[1]
-TWITTER_USERNAME = sys.argv[2]
-TWITTER_PASSWORD = sys.argv[3]
 DATA_DIRECTORY = "data"
 POLITICIANS_FILE = F"{DATA_DIRECTORY}/politicians.json"
 
-scraper = Scraper(TWITTER_EMAIL, TWITTER_USERNAME, TWITTER_PASSWORD)
+scraper = Scraper(session=init_session(), save=False)
 politicians = updateAndGetPoliticians(scraper, POLITICIANS_FILE)
 succeded = getPoliticiansTweets(scraper, DATA_DIRECTORY, politicians)
 infoMessage = "Scraping finished successfully" if succeded else "Failed to scrape all politicians due to the rate limit being reached."
