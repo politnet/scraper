fa347f37427fecc96a51e91c2fa216e2de0cc8f9
piotr <piotrkalotaspam@gmail.com>
2023-10-29 12:06:46 +0100

    Update readme

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ or using your twitter account run the following command:
 python src/main.py account-login -e [twitter_email] -u [twitter_username] -p [twitter_password] add-twitter-account [twitter_username]
 ```
 
-## Scrape tweets
+## Scrape tweets - all politicians
 
 When required politicians are already added, you can scrape the tweets that will be saved in `data/tweets/{politician_twitter_account}.json`. To scrape tweets for all of the politicians from `data/politicians.json` file run the following command:
 
@@ -45,12 +45,26 @@ python src/main.py guest-session scrape-tweets
 or using your twitter account run the following command:
 
 ```bash
-python src/main.py account-login -e [twitter_email] -u [twitter_username] -p [twitter_password] add-twitter-account [twitter_username]
+python src/main.py account-login -e [twitter_email] -u [twitter_username] -p [twitter_password] scrape-tweets
+```
+
+## Scrape tweets - one politician
+
+Instead of all of the politician, if needed only one can be scraped. As previously it will be saved in `data/tweets/{politician_twitter_account}.json`. Remember that the politician has to be present in `data/politicians.json` file. To start scraping run the following command:
+
+```bash
+python src/main.py guest-session scrape-tweets --account-name [twitter_username]
+```
+
+or using your twitter account run the following command:
+
+```bash
+python src/main.py account-login -e [twitter_email] -u [twitter_username] -p [twitter_password] scrape-tweets --account-name [twitter_username]
 ```
 
 **Note:** Using `account-login` will enable you to scrape more tweets than using `guest-session`.
 
-## Schedule tweets scraping
+## Schedule tweets scraping - all politicians
 
 To schedule tweets scraping for all of the politicians from `data/politicians.json` file run the following command:
 
--- src/main.py
+++ src/main.py
@@ -34,39 +34,31 @@ def schedule_tweets_scraping(scraper, interval):
         interval = globals.DEFAULT_SCHEDULER_INTERVAL
     TweetScheduler(scraper).schedule_scraping(interval)
 
-# parser = ArgsParser.get_parser()
-# args = parser.parse_args()
+parser = ArgsParser.get_parser()
+args = parser.parse_args()
 
-# # Login parsing
-# scraper = None
-# if args.login == globals.account_login_cmd:
-#     print("Using twitter account.")
-#     scraper = get_account_scraper(args)
-# elif args.login == globals.guest_session_cmd:
-#     print("Using guest session.")
-#     scraper = get_guest_scraper()
-# else:
-#     parser.error("Invalid login method.")
+# Login parsing
+scraper = None
+if args.login == globals.account_login_cmd:
+    print("Using twitter account.")
+    scraper = get_account_scraper(args)
+elif args.login == globals.guest_session_cmd:
+    print("Using guest session.")
+    scraper = get_guest_scraper()
+else:
+    parser.error("Invalid login method.")
     
-# if args.command == globals.add_twitter_account_cmd:
-#     print("Adding twitter account...")
-#     add_twitter_account(scraper, args.account_name)
-# elif args.command == globals.build_description_queries_cmd:
-#     print("Building description queries...")
-#     build_description_queries(args.num_of_tweets)
-# elif args.command == globals.scrape_tweets_cmd:
-#     print("Scraping tweets...")
-#     scrape_tweets(scraper, args.account_name)
-# elif args.command == globals.schedule_tweets_scraping_cmd:
-#     print("Scheduling tweets scraping..")
-#     schedule_tweets_scraping(scraper, args.interval)
-# else:
-#     parser.error("Invalid command.")
-
-PoliticianUtils.save_politician(        {
-            "user_id": "2858025183",
-            "user_full_name": "Janusz Korwin-Mikke",
-            "user_account_name": "JkmMikke",
-            "description": None,
-            "last_modified": "2023-10-28T09:02:25.365455+00:00"
-        })
+if args.command == globals.add_twitter_account_cmd:
+    print("Adding twitter account...")
+    add_twitter_account(scraper, args.account_name)
+elif args.command == globals.build_description_queries_cmd:
+    print("Building description queries...")
+    build_description_queries(args.num_of_tweets)
+elif args.command == globals.scrape_tweets_cmd:
+    print("Scraping tweets...")
+    scrape_tweets(scraper, args.account_name)
+elif args.command == globals.schedule_tweets_scraping_cmd:
+    print("Scheduling tweets scraping..")
+    schedule_tweets_scraping(scraper, args.interval)
+else:
+    parser.error("Invalid command.")
