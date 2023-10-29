ac4803d500534950156a5363db62f6e72248b49b
piotr <piotrkalotaspam@gmail.com>
2023-10-29 23:44:18 +0100

    Fix syntax

--- src/processor/scheduler.py
+++ src/processor/scheduler.py
@@ -7,14 +7,14 @@ class TweetScheduler:
     def __init__(self, scraper):
         self.scraper = scraper
         
-    def schedule_scraping(self, every_minutes):
+    def schedule_scraping(self, every_minutes, limit):
         print(f"Scheduling scraping every {every_minutes} minutes.")
         schedule.every(int(every_minutes)).minutes.do(
-            lambda: TweetExtractor(self.scraper).get_all_politicians_tweets()
+            lambda: TweetExtractor(self.scraper).get_all_politicians_tweets(limit)
         )
         
         # Start job immaditealy and then every [every_minutes] minutes
-        TweetExtractor(self.scraper).get_all_politicians_tweets()
+        TweetExtractor(self.scraper).get_all_politicians_tweets(limit)
 
         while True:
             schedule.run_pending()
